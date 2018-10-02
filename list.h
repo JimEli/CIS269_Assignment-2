a8ec55b2b2862f0647b795759cb5213ca9fa24a8
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-10-02 06:44:52 -0700

    Update list.h

--- list.h
+++ list.h
@@ -4,19 +4,20 @@
 * Author: James Eli
 * Date: 9/24/2018
 *
-* Minimal Singly Linked List Class.
+* Minimal Singly Linked List Class. Far from complete.
 *
 * Singly linked list constructed of a private node structure. Each node
 * contains two fields (data and next). The data element is templated to
-* be of any type. The list maintinas a head and tail pointer.
-*
-* List member functions (only) include:
+* be of any type. The list maintinas a head and tail pointer. List member
+* functions (only) include:
 *   back()      // Returns element at tail of list.
 *   empty()     // Returns true if list is empty.
-*   push_back() // Adds new node to tail of list.
-*   pop_front() // Removes front element from list.
-*   clear()     // Clears list of all elements.
-*   operator<<  // Overloaded << operator prints list contents.
+*	push_back() // Adds new node to tail of list.
+*	pop_front() // Removes front element from list.
+*	clear()     // Clears list of all elements.
+*   remove()    // Remove element from list.
+*	operator<<  // Overloaded << operator prints list contents.
+*   iterator incorporates only needed functionality for the demo.
 *
 * Notes:
 *  (1) Compiled/tested with MS Visual Studio 2017 Community (v141), and
@@ -28,15 +29,22 @@
 *************************************************************************
 * Change Log:
 *   09/24/2018: Initial release. JME
+*   09/29/2018: Added remove function. JME
+*   09/29/2018: Added very basic iterator support. JME
 *************************************************************************/
 #pragma once
 #ifndef _LIST_H_
 #define _LIST_H_
 
-#include <iostream>  // Output stream.
+#include <iostream> // cout for print functionality.
 
 namespace myList
 {
+
+	// Result (return) of remove function.
+	constexpr auto DELETE_SUCCESS = 0;
+	constexpr auto DELETE_FAIL = -1;
+
 	// Individual list node.
 	template <typename T>
 	struct Node
@@ -71,6 +79,7 @@ namespace myList
 		// Adds new node to tail of list.
 		void push_back(const T& e)
 		{
+			// Delete(s) matching this new are in pop_front/remove.
 			Node<T>* newNode = new Node<T>;
 
 			newNode->element = e;
@@ -119,6 +128,114 @@ namespace myList
 			return os << std::endl;
 		}
 
+		// Find and return node.
+		Node<T>* find(const T& e)
+		{
+			if (this->empty())         // Can't find in empty list.
+				return nullptr;
+
+			Node<T>* node = this->head;
+
+			// Walk list looking for node match.
+			do {
+				if (node->element == e)
+					return node;       // Found it!
+
+				node = node->next;
+
+			} while (node);
+
+			return nullptr;            // Failed to find node.
+		}
+
+		// Remove first occurance of element from list.
+		int remove(T e)
+		{
+			if (this->empty())
+				return DELETE_FAIL;
+
+			Node<T> *node = this->head, *prev = node;
+
+			do {
+				if (node->element == e)
+				{
+					// Decrement count, check if empty?
+					if (!--count)
+					{
+						delete node;
+						this->head = nullptr;
+						this->tail = nullptr;
+						return DELETE_SUCCESS;
+					}
+					// Removing head node?
+					if (node == this->head)
+					{
+						this->head = node->next;
+						delete node;
+						return DELETE_SUCCESS;
+					}
+					// Removing tail node?
+					else if (node == this->tail)
+					{
+						this->tail = prev;
+						prev->next = nullptr;
+						delete node;
+						return DELETE_SUCCESS;
+					}
+					else
+					{
+						prev->next = node->next;
+						delete node;
+						return DELETE_SUCCESS;
+					}
+				}
+
+				prev = node;
+				node = node->next;
+
+			} while (node);
+
+			return DELETE_FAIL;
+		}
+
+		//
+		// Inner iterator class. Member typedefs provided through inheritance from std::iterator.
+		//class iterator : public std::iterator<std::forward_iterator_tag, T, ptrdiff_t, T*, T&>
+		class iterator : public std::iterator<std::forward_iterator_tag, T>
+		{
+		private:
+			Node<T> *pNode;
+
+			// Ctor is private, so only friends can create instances.
+			iterator(Node<T> *n) : pNode(n) { }
+			iterator() : pNode(nullptr) { }
+
+			friend class list;
+
+		public:
+			// Dtor.
+			~iterator() { }
+
+			// Overload comparison operators.
+			bool operator== (const iterator& it) const { return pNode == it.pNode; }
+			bool operator!= (const iterator& it) const { return pNode != it.pNode; }
+
+			// Overload dereference and pointer operators.
+			T& operator* () { return pNode->element; }
+			T* operator-> () { return &pNode->element; }
+
+			// Overload prefix increment operator.
+			iterator& operator++ ()
+			{
+				pNode = pNode->next;
+				return *this;
+			}
+		}; // End iterator inner class.
+
+		// Begin and end iterators.
+		iterator begin() const { return iterator(head); }
+		//iterator end() const { return iterator(tail); }
+		iterator end() const { return iterator(tail->next); }
 	};
 }
 #endif
